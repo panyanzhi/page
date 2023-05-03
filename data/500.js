c2eddc7f4acfdb82ebbc28ab795be94aed371618
yuang <yuang@xkw.cn>
2023-05-03 16:44:17 +0800

    支持临时覆盖2

--- data/500.js
+++ data/500.js
@@ -1,1 +1,1 @@
-(function(){window.dianbo = {content:'抱歉，穿越之门8051正在检修中，稍安勿躁...',date:'2023/04/15 11:17:07',where:'值守机器人'}})()
+(function(){window.dianbo = {content:'抱歉，穿越之门8051正在检修中，稍安勿躁...',date:Date.now(),where:'值守机器人'}})()
