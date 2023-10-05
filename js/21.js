9817146b92abb04678e0c5f80dbdf75f64ddd5cd
panyanzhi <panyanzhi520@icloud.com>
2023-10-05 19:23:38 +0800

    x

--- js/21.js
+++ js/21.js
@@ -31,7 +31,7 @@ function autoDownload (max = 15, free = false) {
     const infos = a.href.split('/')
     const id = infos[infos.length - 1].split('.')[0]
     const nextA = prefix.replace('idx', id)
-    const per = Math.floor(Math.random() * 4) + 2.1 // 返回 1 至 4 之间的数
+    const per = Math.floor(Math.random() * 3) + 2.1 // 返回 1 至 4 之间的数
     setTimeout(() => { window.open(nextA, '_blank') }, per * i * 1000)
   }
 }
